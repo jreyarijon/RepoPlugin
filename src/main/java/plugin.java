332757b0f0964d37906a79659e4f34d027a7ccf9
Josemolamazo <Jreyarijon@danielcastelao.org>
2020-02-28 20:11:48 +0100

    Subido con fichero de config

--- src/main/java/plugin.java
+++ src/main/java/plugin.java
@@ -7,15 +7,14 @@ import java.io.IOException;
 
 public class plugin {
     public static void main(String[] args) throws IOException {
-        String token = JOptionPane.showInputDialog(null, "Teclee token");
 
-        GitHub github = new GitHubBuilder().withOAuthToken(token).build();
+        String nome = JOptionPane.showInputDialog("Nombre del repositorio");
+        String descripcion = JOptionPane.showInputDialog("Descripcion");
+        GitHub github = GitHubBuilder.fromPropertyFile("C:\\Users\\Josemolamazo\\IdeaProjects\\pluginGitCd\\.github").build();
+        GHRepository repo = github.createRepository(nome,descripcion,
+                "https://github.com/jreyarijon",false);
 
-        GHRepository repo = github.createRepository(
-                "PluginTresRamasToken","Tiene tres ramas",
-                "https://github.com/jreyarijon",false/*private*/);
-        repo.addCollaborators(github.getUser("damianCastelao"));
-
+        repo.addCollaborators(github.getUser("damiancastelao"));
     }
 
 }
