b5f3f59ae49175e058feeec20aae3565fe690971
Josemolamazo <Jreyarijon@danielcastelao.org>
2020-02-28 14:43:55 +0100

    Subido con token

--- src/main/java/plugin.java
+++ src/main/java/plugin.java
@@ -7,12 +7,12 @@ import java.io.IOException;
 
 public class plugin {
     public static void main(String[] args) throws IOException {
-        String nome = JOptionPane.showInputDialog(null, "Teclee usuario");
-        String contrasenha = JOptionPane.showInputDialog(null,"Teclee contrasenha");
-        GitHub github = new GitHubBuilder().withPassword(nome, contrasenha).build();
+        String token = JOptionPane.showInputDialog(null, "Teclee token");
+
+        GitHub github = new GitHubBuilder().withOAuthToken(token).build();
 
         GHRepository repo = github.createRepository(
-                "PluginTresRamas","Tiene tres ramas",
+                "PluginTresRamasToken","Tiene tres ramas",
                 "https://github.com/jreyarijon",false/*private*/);
         repo.addCollaborators(github.getUser("damianCastelao"));
 
