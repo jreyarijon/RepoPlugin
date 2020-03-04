4eaa8bbf4297918d562065d4a8e24ef5df38c822
Josemolamazo <Jreyarijon@danielcastelao.org>
2020-03-04 14:32:32 +0100

    Comentado

--- src/main/java/plugin.java
+++ src/main/java/plugin.java
@@ -7,14 +7,17 @@ import java.io.IOException;
 
 public class plugin {
     public static void main(String[] args) throws IOException {
+        //Pedimos el nombre y la contraseña por teclado
         String nome = JOptionPane.showInputDialog(null, "Teclee usuario");
         String contrasenha = JOptionPane.showInputDialog(null,"Teclee contrasenha");
+        //Introducimos el nombre y la contraseña para dar permiso a acceder a mi cuenta de github
         GitHub github = new GitHubBuilder().withPassword(nome, contrasenha).build();
-
+        //Creamos el repositorio introduciendo su nombre y la descripcion
         GHRepository repo = github.createRepository(
                 "PluginTresRamas","Tiene tres ramas",
                 "https://github.com/jreyarijon",false/*private*/);
-        repo.addCollaborators(github.getUser("damianCastelao"));
+        //Añadimos un colaborador
+        repo.addCollaborators(github.getUser("damiancastelao"));
 
     }
 
